3b32acf94d0fc241041143ef1ad5577ca3164782
micaeprado <micaelaprado94@gmail.com>
2019-05-28 19:03:08 -0300

    usuarios

--- server/server.js
+++ server/server.js
@@ -18,39 +18,6 @@ app.get('/', (req, res) => {
     res.json('Hello World')
 })
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario')
-})
-
-app.post('/usuario', (req, res) => {
-
-    let body = req.body;
-
-    if (body.nombre == undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'Debe ingresar el nombre'
-        })
-    }
-
-    res.json({
-        persona: body
-    })
-})
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario')
-})
-
-
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando puerto: `, process.env.PORT);
 })
