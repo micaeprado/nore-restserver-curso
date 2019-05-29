94b68aa6fe97e3395230c0ced4a124393d7b8679
micaeprado <micaelaprado94@gmail.com>
2019-05-29 17:56:52 -0300

    put y get

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,15 +1,45 @@
 const express = require('express');
-const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
+const Usuario = require('../models/usuario');
+
 
 
 const app = express();
 
 
 app.get('/usuario', (req, res) => {
-    res.json('get usuario')
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    Usuario.find({}, 'nombre email role estado imagen')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({}, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+            });
+
+        });
 })
 
+/**
+ * Devuelve los datos cargados
+ */
 app.post('/usuario', (req, res) => {
 
     let body = req.body;
@@ -37,12 +67,28 @@ app.post('/usuario', (req, res) => {
 
 })
 
+/**
+ * Actulización de registro
+ */
 app.put('/usuario/:id', (req, res) => {
     let id = req.params.id;
+    //agarra del body solamente los elementos que estan en el arreglo (los q se permite modificar)
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
-    res.json({
-        id
-    });
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    })
+
 });
 
 app.delete('/usuario', (req, res) => {
