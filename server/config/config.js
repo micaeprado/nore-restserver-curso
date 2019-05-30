40eba97cb29b850465883d6de2e56e5a0b29d8e4
micaeprado <micaelaprado94@gmail.com>
2019-05-29 21:33:54 -0300

    ad

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,24 @@
 //=========================
 
 process.env.PORT = process.env.PORT || 3000; //variable global
+
+
+//=========================
+// ENTORNO
+//=========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//=========================
+// BASE DE DATOS
+//=========================
+
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb+srv://micaeprado:jWTCMTj9qYdEm6J@cluster0-l5dgu.mongodb.net/cafe';
+// }
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ app.use(bodyParser.json());
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}, (err, res) => {
     if (err) throw err;
     console.log("Base de datos ONLINE");
 });
